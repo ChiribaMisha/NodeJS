1ced75d4d6d558f4d8c652fa72c1bea74ae17822
Misha Chiriba <you@example.com>
2021-03-01 17:28:11 +0200

    NodeJS

--- Csv_Node/start1/routes/index.js
+++ Csv_Node/start1/routes/index.js
@@ -4,17 +4,22 @@ const axios = require('axios');
 const multer = require('multer');
 const upload = multer();
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
+const { error } = require('console');
 
 router.get('/', (req, res) => {
   res.render('index');
 });
 
 router.post('/', upload.none(), (req, res) => {
-  fs.writeFile('./csv/1.csv', `${req.body.author};${req.body.date};${req.body.textarea}\n ${req.body.author},${req.body.date},${req.body.textarea}`, error => {
+  fs.writeFile('./csv/1.csv', `${req.body.author};${req.body.date};${req.body.textarea}\n`, error => {
     if (error) throw error;
     res.send('Сохранено успешно');
   });
+
+  fs.appendFile('./csv/1.csv', `${req.body.author},${req.body.date},${req.body.textarea}`, error => {
+    if (error) throw error;
+  })
 });
 
 router.get('/csv/1.csv', (req, res) => {
